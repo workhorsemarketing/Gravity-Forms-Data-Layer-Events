6b2f1ef61375286073c2529ea3b7e1dc6c28acd6
Thomas Bacon <thomas@workhorsemkt.com>
2024-10-30 10:54:30 -0500

    added email addresses
    added hashed and plain text email addresses to dl as well as updated meta data to reflect new agency name / url

--- gravity-forms-data-layer-events.php
+++ gravity-forms-data-layer-events.php
@@ -2,45 +2,68 @@
 /*
 Plugin Name: Gravity Forms Data Layer Events
 Plugin URI: https://github.com/workhorsemarketing/Gravity-Forms-Data-Layer-Events
-Description: Fires off a Google Tag Manager data layer event <code>gf_form_submission</code> and includes parameters <code>gf_form_id</code> and <code>gf_form_name</code> when a Gravity Form is submitted. Works with all confirmation types (Ajax, text, Redirect, new page).
-Author: Workhorse Marketing
-Version: 1.1
-Author URI: https://www.workhorsemkt.com/
+Description: Fires off a Google Tag Manager data layer event <code>gf_form_submission</code> and includes parameters <code>gf_form_id</code>, <code>gf_form_name</code>, and <code>email</code> when a Gravity Form is submitted. Works with all confirmation types (Ajax, text, Redirect, new page).
+Author: Workhorse
+Version: 1.2
+Author URI: https://www.builtbyworkhorse.com/
 */
+
 add_filter( 'gform_confirmation', function ( $confirmation, $form, $entry, $ajax ) {
 
-	if ($entry['status'] !== 'active') {
-		return $confirmation;
-	}
+    if ($entry['status'] !== 'active') {
+        return $confirmation;
+    }
 
-	$form_title = $form['title'];
-	$redirect = "";
-	if(!empty($confirmation['redirect'])){
-		$redirect .= ',
-			"eventCallback": () => {
-				window.location.replace("'.esc_url_raw($confirmation['redirect']).'");
-			}
-		';
-	}
-	$js = '
-		if(window.self === window.top){
-			window.dataLayer = window.dataLayer || [];
-			window.dataLayer.push({
-				"event": "gf_form_submission",
-				"gf_form_id": ' . $form['id'] . ',
-				"gf_form_name": "' . esc_js($form_title) . '"
-				' . $redirect . '
-			});
-		}
-	';
+    $form_title = $form['title'];
+    $redirect = "";
 
-	$newConfirmation = is_string($confirmation) ? $confirmation : '';
+    // Collect email fields and hashed values from entry data
+    $email_js_array = '';
+    $email_count = 1;
 
-	if(!empty($confirmation['redirect'])){
-		// $js .= "setTimeout(() => window.location.replace('".esc_url_raw($confirmation['redirect'])."'), 10000);";
-	}
+    foreach ( $form['fields'] as $field ) {
+        if ( $field->type === 'email' ) {
+            $email_field_id = $field->id;
+            $email_value = rgar( $entry, $email_field_id );
 
-	$newConfirmation .= GFCommon::get_inline_script_tag( $js );
+            if ( !empty($email_value) ) {
+                $hashed_email = hash('sha256', $email_value);
+                
+                $email_js_array .= '"email' . $email_count . '": "' . esc_js($email_value) . '",';
+                $email_js_array .= '"email' . $email_count . '_hashed": "' . esc_js($hashed_email) . '",';
+                $email_count++;
+            }
+        }
+    }
 
-	return $newConfirmation;
+    if(!empty($confirmation['redirect'])){
+        $redirect .= ',
+            "eventCallback": () => {
+                window.location.replace("'.esc_url_raw($confirmation['redirect']).'");
+            }
+        ';
+    }
+    
+    $js = '
+        if(window.self === window.top){
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({
+                "event": "gf_form_submission",
+                "gf_form_id": ' . $form['id'] . ',
+                "gf_form_name": "' . esc_js($form_title) . '",
+                ' . $email_js_array . '
+                ' . $redirect . '
+            });
+        }
+    ';
+
+    $newConfirmation = is_string($confirmation) ? $confirmation : '';
+
+    if(!empty($confirmation['redirect'])){
+        // $js .= "setTimeout(() => window.location.replace('".esc_url_raw($confirmation['redirect'])."'), 10000);";
+    }
+
+    $newConfirmation .= GFCommon::get_inline_script_tag( $js );
+
+    return $newConfirmation;
 }, 10, 4 );
